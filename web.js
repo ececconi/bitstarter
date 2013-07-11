99d169e5df120611867abcf262c5d94e54db38a6
ececconi <emiliocecconi@gmail.com>
2013-07-11 01:18:33 +0000

    Debugging

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buf = fs.readFileSync('index.html', 'utf8');
-  response.send(buf.tostring('utf8'));
+  var content = fs.readFileSync('index.html','utf8');
+  response.send(content.toString());
 });
 
 
